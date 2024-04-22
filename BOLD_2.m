a024eb5fa763cd8bf66a22ed6585e357483d2516
sbhatt12 <sbhatt5@wisc.edu>
2024-04-22 11:58:44 -0500

    changed directory settings to local

--- BOLD_2.m
+++ BOLD_2.m
@@ -1,16 +1,16 @@
-addpath("GitHub\rat-resonator-Matlab\src\");
+addpath("src\");
 
-t2_slice = get_t2('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3749_T2post\1\3749_',15);
+t2_slice = get_t2('2023_06_21_Suyash_Rat_surg_4_10_injections\3749_T2post\1\3749_',15);
 size_cols = size(t2_slice,2);
 size_rows = size(t2_slice,1);
 
-baseline_t = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3738_Bold_post1\1\3738_',11:86);
-BOLD_t(:,:,1:500) = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3738_Bold_post1\1\3738_',1:500);
-BOLD_t(:,:,501:1000) = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3739_Bold_post2\1\3739_',1:500);
-BOLD_t(:,:,1001:1500) = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3740_Bold_post3\1\3740_',1:500);
-BOLD_t(:,:,1501:2000) = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3741_Bold_post4\1\3741_',1:500);
-BOLD_t(:,:,2001:2500) = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3742_Bold_post5\1\3742_',1:500);
-BOLD_t(:,:,2501:3000) = load_BOLD('D:\Data\2023_06_21_Suyash_Rat_surg_4_10_injections\2023_06_21_Suyash_Rat_surg_4_10_injections\3743_Bold_post6\1\3743_',1:500);
+baseline_t = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3738_Bold_post1\1\3738_',11:86);
+BOLD_t(:,:,1:500) = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3738_Bold_post1\1\3738_',1:500);
+BOLD_t(:,:,501:1000) = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3739_Bold_post2\1\3739_',1:500);
+BOLD_t(:,:,1001:1500) = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3740_Bold_post3\1\3740_',1:500);
+BOLD_t(:,:,1501:2000) = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3741_Bold_post4\1\3741_',1:500);
+BOLD_t(:,:,2001:2500) = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3742_Bold_post5\1\3742_',1:500);
+BOLD_t(:,:,2501:3000) = load_BOLD('2023_06_21_Suyash_Rat_surg_4_10_injections\3743_Bold_post6\1\3743_',1:500);
 
 baseline_t_resized = imresize(baseline_t,[size_rows size_cols]);
 BOLD_t_resized = imresize(BOLD_t,[size_rows size_cols]);
@@ -41,3 +41,11 @@ for x = 1:size_cols
         BOLD_t_filtered(y,x,:) = pixel_filtered;
     end
 end
+
+BOLD_t_full = BOLD_t_filtered./mean(baseline_t_filtered,3);
+BOLD_t_roi(:,2) = squeeze(mean(BOLD_t_full(70:72,124:126,:),[1 2]));
+figure
+spectrogram(BOLD_t_roi(:,2),50,40,1000,3000/3600,'yaxis')
+xlim([20 40])
+clim([-15 0])
+ylim([10 160])
--- ion_5.m
+++ ion_5.m
@@ -1,16 +1,16 @@
-addpath("GitHub\rat-resonator-Matlab\src\");
+addpath("src\");
 
-t2_slice = get_t2('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3174_t2_post\1\3174_',14);
+t2_slice = get_t2('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3174_t2_post\1\3174_',14);
 size_cols = size(t2_slice,2);
 size_rows = size(t2_slice,1);
 
-baseline_t = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3166_Bold_post_1\1\3166_',11:86);
-BOLD_t(:,:,1:500) = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3166_Bold_post_1\1\3166_',1:500);
-BOLD_t(:,:,501:1000) = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3167_Bold_post_2\1\3167_',1:500);
-BOLD_t(:,:,1001:1500) = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3168_Bold_post_3\1\3168_',1:500);
-BOLD_t(:,:,1501:2000) = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3169_Bold_post_4\1\3169_',1:500);
-BOLD_t(:,:,2001:2500) = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3170_Bold_post_5\1\3170_',1:500);
-BOLD_t(:,:,2501:3000) = load_BOLD('D:\Data\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3173_Bold_post_6\1\3173_',1:500);
+baseline_t = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3166_Bold_post_1\1\3166_',11:86);
+BOLD_t(:,:,1:500) = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3166_Bold_post_1\1\3166_',1:500);
+BOLD_t(:,:,501:1000) = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3167_Bold_post_2\1\3167_',1:500);
+BOLD_t(:,:,1001:1500) = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3168_Bold_post_3\1\3168_',1:500);
+BOLD_t(:,:,1501:2000) = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3169_Bold_post_4\1\3169_',1:500);
+BOLD_t(:,:,2001:2500) = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3170_Bold_post_5\1\3170_',1:500);
+BOLD_t(:,:,2501:3000) = load_BOLD('C:\Users\suyas\Documents\GitHub\rat-resonator-Matlab\2023_05_02_Suyash_Rat_Surgery_03_08_4ap\3173_Bold_post_6\1\3173_',1:500);
 
 baseline_t_resized = imresize(baseline_t,[size_rows size_cols]);
 ion_t_resized = imresize(BOLD_t,[size_rows size_cols]);
